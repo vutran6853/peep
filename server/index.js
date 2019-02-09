3fd265f7c76dee82ee5844a2a798a1bb512972c3
Vu Tran <vutran6853@gmail.com>
2019-02-09 00:47:38 -0600

    clean up

--- server/index.js
+++ server/index.js
@@ -23,21 +23,12 @@ app.use(cors());
 const { getAllUser, registerUser } = require('./controller/peepApiController');
 
 //// Connect to PostgreSQL Database and define 'db' on app
-// massive.connectSync({ connectionString: url })
-// .then((dbInstance) => {
-//   console.log('Connet to Database...', dbInstance);
-//   app.set('db', dbInstance)
-// })
-// .catch((error) => console.log(`Danger unable to connect to Database`));
-// console.log(massive);
-
-massive('postgres://oesrnqnohtgkxj:142ab8760cd3d878c5f170325ab0bf1875d89516386cdcc7d744d730ebbcd81d@ec2-54-163-246-159.compute-1.amazonaws.com:5432/d6om9fi9vc9ekq?ssl=true')
+massive(process.env.DB_CONNECT_STRING)
 .then(dbInstance => {
   // console.log(`copy of DB:`, dbInstance)
   app.set('db', dbInstance)
   monitor.attach(dbInstance.driverConfig);
 });
-
 
 
 ////  User Endpoint RESTFull API
